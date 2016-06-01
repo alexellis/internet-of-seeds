53e8322244d91075a4bc5adb043360efac309ed4
Alex Ellis <alexellis2@gmail.com>
2016-06-01 08:46:17 +0000

    Removing temp file

--- copyer.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-def copy(source, destination):
-    with open(source, 'rb') as f1:
-        with open(destination, 'wb') as f2:
-            f2.write(f1.read())
-
-copy(sys.argv[1], sys.argv[2])
